7f23e54abd25c8f5aea4531584ef35e0a439df83
muniky <muniky.santos@alunos.sc.senac.br>
2023-11-29 21:28:17 -0300

    contatos.map

--- src/componentes/consultaContato.js
+++ src/componentes/consultaContato.js
@@ -1,23 +1,37 @@
 import Pesquisa from "./pesquisa";
 
-export default function ConsultaContato(){
-return(
-<>
-<h2 className="text-center">Consultar Contato</h2>
-<hr></hr>
-<Pesquisa/>
-<table className="table">
-    <thead>
-        <tr>
-            <th>Id</th>
-            <th>Nome</th>
-            <th>E-mail</th>
-        </tr>
-    </thead>
-    <tbody>
+export default function ConsultaContato() {
+    const contatos = [
+        { id: 1, nome: 'maria', email: 'maria@gmail.com' },
+        { id: 2, nome: 'josé', email: 'jose@gmail.com' }
 
-    </tbody>
-</table>
-</>
-)
+    ]
+    return (
+        <>
+            <h2 className="text-center">Consultar Contato</h2>
+            <hr></hr>
+            <Pesquisa />
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        contatos.map(ct =>
+                            <tr>
+                                <td> {ct.id}</td>
+                                <td> {ct.nome}</td>
+                                <td> {ct.email}</td>
+                            </tr>
+                        )
+                    }
+
+                </tbody>
+            </table>
+        </>
+    )
 }
